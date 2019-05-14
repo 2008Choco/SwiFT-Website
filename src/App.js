3d5f0029e09552465efc91ec6484d2c5440e13d7
Parker Hawke <hawkeboyz2@hotmail.com>
2019-05-14 16:40:01 -0400

    Finalize the landing page with text and contact form. Landing page done!
    (he said to himself, knowing there are still some things that need to be tweaked. Though at least it looked nice and he could relax and watch some YouTube that night)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import './App.css';
+import './styles/landing-contact.css';
 
 import NavBar from "./components/NavBar";
 
@@ -8,6 +9,22 @@ export default class App extends Component {
     return (
       <React.Fragment>
         <NavBar/>
+
+        <div className="front">
+          <div className="body-text">
+            <p id="header">Team SwiFT</p>
+            <p id="content">Welcome to the official Team SwiFT Website. Everything you need to know about us can be found without a hassle.</p>
+          </div>
+
+          <div className="contact-container">
+            <p>Send us a message!</p>
+            <form className="contact-form">
+              <input type="text" name="name" placeholder="Name" required/>
+              <textarea name="description" placeholder="Your Message" required/>
+              <input type="submit" name="submit"/>
+            </form>
+          </div>
+        </div>
       </React.Fragment>
     );
   };
