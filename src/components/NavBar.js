31344d908dc9b5816241cd6b2e2a49a36ccd6bdb
Parker Hawke <hawkeboyz2@hotmail.com>
2019-05-15 21:17:25 -0400

    Simplify the previously-committed, over-engineered logo flipping

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 import NavBarElementList from './NavBarElementList';
 
 import '../styles/nav.css'
-import '../styles/flipper.css';
 
 const logo = require("../assets/swift-logo.png");
 
@@ -11,14 +10,7 @@ export default class NavBar extends Component {
     return (
       <div id="nav-wrapper">
         <NavBarElementList side="left" elements={['Home', 'Recruitment', 'News', 'Roster']}/>
-
-        <div className="flip-container">
-          <div className="flipper">
-            <img className="swift-logo" id="front" src={logo} alt="SwiFT Logo"/>
-            <img className="swift-logo" id="back" src={logo} alt="SwiFT Logo"/>
-          </div>
-        </div>
-        
+        <img className="swift-logo" id="front" src={logo} alt="SwiFT Logo"/>
         <NavBarElementList side="right" elements={['Merchandise', 'About', 'Contact', 'Media']}/>
       </div>
     )
