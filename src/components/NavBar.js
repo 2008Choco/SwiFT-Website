21ca55760f51a8525f6be3624c6044870beae18c
Parker Hawke <hawkeboyz2@hotmail.com>
2019-05-15 20:59:21 -0400

    Add a little spin animation to the SwiFT logo in the nav bar
    It's the little things that count :)

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import NavBarElementList from './NavBarElementList';
 
 import '../styles/nav.css'
+import '../styles/flipper.css';
 
 const logo = require("../assets/swift-logo.png");
 
@@ -10,7 +11,14 @@ export default class NavBar extends Component {
     return (
       <div id="nav-wrapper">
         <NavBarElementList side="left" elements={['Home', 'Recruitment', 'News', 'Roster']}/>
-        <img src={logo} alt="SwiFT Logo" id="swift-logo"/>
+
+        <div className="flip-container">
+          <div className="flipper">
+            <img className="swift-logo" id="front" src={logo} alt="SwiFT Logo"/>
+            <img className="swift-logo" id="back" src={logo} alt="SwiFT Logo"/>
+          </div>
+        </div>
+        
         <NavBarElementList side="right" elements={['Merchandise', 'About', 'Contact', 'Media']}/>
       </div>
     )
