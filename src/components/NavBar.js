898e55d56e2abb06009b97fc1d3241959b64bc9b
Parker Hawke <hawkeboyz2@hotmail.com>
2019-05-17 22:37:12 -0400

    Declare an id instead of a class for the swift logo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,7 +10,7 @@ export default class NavBar extends Component {
     return (
       <div id="nav-wrapper">
         <NavBarElementList side="left" elements={['Home', 'Recruitment', 'News', 'Roster']}/>
-        <img className="swift-logo" id="front" src={logo} alt="SwiFT Logo"/>
+        <img id="swift-logo" src={logo} alt="SwiFT Logo"/>
         <NavBarElementList side="right" elements={['Merchandise', 'About', 'Contact', 'Media']}/>
       </div>
     )
