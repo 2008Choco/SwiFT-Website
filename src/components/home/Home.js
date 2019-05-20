3d13547824401886755c00f25d58fe9bdfb9af32
Parker Hawke <hawkeboyz2@hotmail.com>
2019-05-20 11:53:41 -0400

    Let Home.js be a function-based component

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -3,24 +3,22 @@ import React, { Component } from 'react'
 import '../../styles/Home.css';
 import '../../styles/landing-contact.css';
 
-export default class Home extends Component {
-  render() {
-    return (
-        <div className="front">
-          <div className="body-text">
-            <p id="header">Team SwiFT</p>
-            <p id="content">Welcome to the official Team SwiFT Website. Everything you need to know about us can be found without a hassle.</p>
-          </div>
+export default function Home() {
+  return (
+    <div className="front">
+      <div className="body-text">
+        <p id="header">Team SwiFT</p>
+        <p id="content">Welcome to the official Team SwiFT Website. Everything you need to know about us can be found without a hassle.</p>
+      </div>
 
-          <div className="contact-container">
-            <p>Send us a message!</p>
-            <form className="contact-form">
-              <input type="text" name="name" placeholder="Name" required/>
-              <textarea name="description" placeholder="Your Message" required/>
-              <input type="submit" name="submit"/>
-            </form>
-          </div>
-        </div>
-    );
-  };
+      <div className="contact-container">
+        <p>Send us a message!</p>
+        <form className="contact-form">
+          <input type="text" name="name" placeholder="Name" required/>
+          <textarea name="description" placeholder="Your Message" required/>
+          <input type="submit" name="submit"/>
+        </form>
+      </div>
+    </div>
+  );
 }
